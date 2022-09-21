94e0e6082ebd6d5d13c965074d47b8d92635d3a3
Edmir Lôbo <ejuniorls@outlook.com>
2022-09-20 23:31:30 -0300

    fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faAngular, faBootstrap, faFontAwesome } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'admin-template';
+  title = 'Hello, Angular with Bootstrap and Font Awesome!';
+
+  faAngular = faAngular;
+  faBootstrap = faBootstrap;
+  faFontAwesome = faFontAwesome;
 }
